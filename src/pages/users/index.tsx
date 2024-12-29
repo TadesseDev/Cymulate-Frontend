17cea767f7eed80f328cb3e40fcbf77192625846
tadessedev <tadessedev@gmail.com>
2024-12-29 12:59:42 +0300

    fetch and display users

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -10,27 +10,31 @@ function Users() {
   const { users, error, isLoading } = useSelector((state: RootState) => state.user);
   useEffect(() => {
     dispatch(fetchUsers());
-    dispatch(
-      createUser({
-        name: "first user",
-        password: "dummy password",
-        email: "dummyemail@samplemail.com",
-      })
-    );
+    // dispatch(
+    //   createUser({
+    //     name: "first user",
+    //     password: "dummy password",
+    //     email: "dummyemail@samplemail.com",
+    //   })
+    // );
   }, [dispatch]);
 
+  console.log(users.length, users);
   return (
     <div>
       {isLoading && <p>Loading users...</p>}
       {error && <p>Error: {error}</p>}
       {!isLoading && !error && (
         <ul>
-          {users.length &&
+          {users.length ? (
             users.map((user) => (
               <li key={user._id}>
-                {user.name} - {user.password}
+                {user.name} - {user.password} - {user.email}
               </li>
-            ))}
+            ))
+          ) : (
+            <div>No user</div>
+          )}
         </ul>
       )}
     </div>
